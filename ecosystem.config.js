8818d4c1cffdc8ac77237b839c9255b7f17dd713
sayed132 <rafsansayed132@gmail.com>
2025-08-20 23:44:08 +0600

    ecosetem change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps: [
         {
             name: 'johnchen0213-server',
-            port: 12011,
             script: './dist/server.js',
             args: 'start',
             env: {
